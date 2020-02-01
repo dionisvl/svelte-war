11a17a4ddbb43941d49e96ebb6323dfcd157527c
den <dden_vlad@mail.ru>
2020-02-01 21:32:34 +0300

    Добавлен эффект смерти противников

--- src/components/Enemy.svelte
+++ src/components/Enemy.svelte
@@ -1,9 +1,13 @@
 <script>
-// В переменную enemy будем принимать объект, описывающий врага
-export let enemy;
+
+    // В переменную enemy будем принимать объект, описывающий врага
+    export let enemy;
+
+    import { fade } from 'svelte/transition';
+    let visible = true;
 </script>
 
 // Отобразим прямоугольник с врагом, выполнив трансформацию по текущим координатам.
-<g transform={`translate(${enemy.x}, ${enemy.y})`} >
-<rect width="30" height="30" fill="#212121" />
-    </g>
+<g transition:fade transform={`translate(${enemy.x}, ${enemy.y})`}>
+    <rect width="30" height="30" fill="#212121"/>
+</g>
